637bf3ec7c36a54d06e2d6cb2f8895e5f4f32afa
ultra-mongolia <tsbayarbold2@gmail.com>
2024-12-29 16:23:00 +0800

    scp cosmology

--- bayarbold.php
+++ bayarbold.php
@@ -1,1 +1,51 @@
 warhammer 40k multiverse
+<p>hello world</p>
+<p>Universes
+
+Infinite layers of existence beyond conceptual duality and distinction between existence and non-existence, infinite fractal hierarchy of space-time where each level of existence conceptually transcends the infinite hierarchy of space-time below it, absolutely infinite in size, contains Set Theory, contains the Ultimate Ensemble, contains the Theory of Everything, contains uncountably infinite dimensions, with there being R>F for each higher dimension, contains Plato's Theory of Forms, contains Platonic Concepts, everything in the universe has to be conceptual, higher plane in the universe contains all concepts, is beyond space-time, each universe is layered on top of each other, every single one seeing the universe below as fiction, the “True Universe” is an infinitesimal part of a greater universe
+
+There are an infinite amount of sets containing an infinite amount of sets containing another infinite amount of sets… containing an infinite amount of universes. Universes also exist in a form similar to a jenga tower, with the hierarchy reaching Berkeley's Cardinal alongside contradicting the axiom of choice.
+
+Multiverses
+
+Contains those previous multiverses, runs on both Pataphysics and Philosophy, infinitely layered, has a hierarchy that views the lower multiverse as infinitesimal, containing fractals within fractals, there are infinite multiverses,
+
+These infinite multiverses are themselves a fraction of a greater metaverse
+
+Noosphere
+
+Contains beings the size of Alephs, is not actually the fifth spacial dimension, just an abstract unit, is the collective consciousness of all humans, is also their collective unconsciousness and subconscious, is Plato's World of Forms, all concepts, thoughts and memetic phenomena are only a fraction of what exists in the Noosphere, is the carrier of Plato's Forms, far beyond all dimensions of the universe, the Anti-Nöosphere is the set of all thoughts which humans are incapable of conceiving
+
+In the Antimemetics Division series, at least one omniverse exists within the confines of the Noosphere and Tufto's Proposal implies that it is "conceptual" in nature in a somewhat similar way to the Noosphere SCP-3125 can take over. The fact that SCP-3125 does not take the role of being the narrator implies that there are multiple omniverses and Swann entities immune to the being's effects within a single Noosphere. The AIAD canon posits another, larger Noospheric omniverse that also extends to universes created by the Cybersphere, a similar conceptual mental plane made up of all the information only computers and computerized life can understand.
+
+In the old lore, memes were more likely to be entirely informational and not powerful rather than shoot beams or change physical things. However, newer lore has made beings such as the Christian God combinations of multiple memes, although these gods' ambiguous nature and control over the world's history is somwewhat similar to many old-style memes. Also, even the relatively weak Fred, a mere sentient meme, is comparable to SCP-2719, which is comparable to but weaker than weaker versions of Alagadda and SCP-3812. Singular memes or memetic entities can take different forms like conceptual being for Fred and mere pieces of information like most memes.
+
+The Noosphere is above English branch SCP-3812.
+
+Semiosphere
+
+Is the medium which information travels before being perceived by the Noosphere, contains anomalous laws of mathematics and logic and things inside it can manipulate the laws of mathematics, it contains universal axioms that do not abide by human logic, responsible for concepts such as absolute trivialism and paraconsistent logic
+
+Infosphere
+
+Is the collective of all information, dwarfs the Noosphere to an incalculable degree and contains it, is a hyper-conceptual reality, concepts that can be conceptualized by the human mind only exist as cross-sections of much larger Infospheric superstructures, which are themselves incapable of being known or entertained abstractly, contains all possible ideas, the entire Foundation universe is merely a simulation on an extradimensional computer and contains the 13 Constants which define the way the Foundation universe operates
+
+Patasphere
+
+Is IHP's Proposal; "Story of Your Life", is responsible for the causality in a narrative layer, is the set of all stories conceivable by humans in a given storyline, more proof for above, is the set of all possible stories related to a single canon, acting as its backbones and dictating all of its parameters, being described as the "infinitely complicated story" of which all lower levels of reality are simply imitations, it contains all possible versions and variations of all objects in a canon, as it exists as the canvas on which author-entities impose their will, it exists on a level beyond all ideas and above standard conceptualization, as it is confirmed to be the fifth conceptual tierspace
+
+Patasphere is Tier 5 of the Conceptual Hierarchy, while there are 256,713 Tiers in total. Each Tier is harder to reach than the last. Since there is a gap between the physical reality and the Noosphere, as well as the gap between Anti-Noosphere and the Patasphere, this means that later Tiers follows a gap just as inaccessible
+
+The beings here should be stronger than the Swann entities within the Noosphere, although the Alagaddans, the weakest here, are often bound by narrative conventions and tropes.
+
+Narratives
+
+All of that is contained inside of a narrative layer, which there are infinite of inside a narrative. Narratives are stacked ontop of one another and are in a hierarchy where an upper narrative would create a lower one and so on so forth. Each narrative contains the Extended Modal Realism. A narrative ladder/stack/echelon contains an infinite amount of narratives.
+
+Database
+
+The Database, from the Department of Deletions stories,
+
+Phantom Bubbles
+
+Contains a Narrative Echelon, a World View and also contains a Grand Narrative Echelon which contains the three Narrative Domains. The Narrative Domains are basically a setting and its fanmade works. World Views contains, official or not, an author's viewpoint on how the SCP World works, which is represented in the Delusional Domain as a Phantom Bubble. There are an immeasurable amount of Phantom Bubbles.</p>
